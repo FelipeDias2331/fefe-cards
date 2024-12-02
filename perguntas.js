6b71ed96533c704f2d95950a7c3d88ce7b0541f2
Felipe Dias (;-;) <garoucum@gmail.com>
2024-12-02 14:10:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Ciencia da Natureza',
+    'Como se diz oi em Inglês?',
+    ' Oi em ingles é HI'
+
+)
+
+criaCartao(
+    'Ciencia da Natureza',
+    'Quantos ossos temos no corpo?',
+    'Um adulto tem, em média, 206 ossos. Os bebês nascem com cerca de 270 ossos, mas alguns se fundem ao longo do crescimento.'
+)
+
+criaCartao(
+    'Ciencia da Natureza',
+    'Qual a capital da França?',
+'A capital da França é Paris'
+)
+
+criaCartao(
+    'Ciencia da Natureza',
+    'Por que sentimos coceira?',
+    'A coceira é uma reação do corpo para alertar sobre possíveis irritações na pele, como picadas de insetos ou substâncias irritantes. Coçar ajuda a remover a causa, mas também estimula o cérebro, liberando endorfinas'
+)
